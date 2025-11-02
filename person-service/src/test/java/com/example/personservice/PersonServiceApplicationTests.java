0e5484829632bebeb9aa0d16a40c0d5a3541eb7f
Aleksandr Belov <uka_ka@inbox.ru>
2025-11-02 15:35:11 +0300

    fix context loads person service

--- person-service/src/test/java/com/example/personservice/PersonServiceApplicationTests.java
+++ person-service/src/test/java/com/example/personservice/PersonServiceApplicationTests.java
@@ -1,10 +1,11 @@
 package com.example.personservice;
 
+import com.example.personservice.config.PGSQLContainer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PersonServiceApplicationTests {
+class PersonServiceApplicationTests extends PGSQLContainer {
 
     @Test
     void contextLoads() {
